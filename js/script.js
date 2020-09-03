b4e876b070de3be9022d56f5aec54704115a52fb
Davide Preti <davide.preti86@gmail.com>
2020-09-03 18:17:30 +0200

    Aggiunta logica per mostrare / nascondere il menu a tendina tramite js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $("nav li a").on({
+    $("nav li a ").on({
 
         mouseenter: function () {
             $(this).addClass("active");
@@ -13,8 +13,21 @@ $(document).ready(function () {
     });
     // fine selettore
 
+    // quando il mouse va sopra il tag a, allora mostra il menu a tendina
+    $(".nav_menu__item > a").on({
 
+        mouseenter: function () {
+            $(this).next().css("display", "block");
+        }
+    });
 
+    // quando il mouse esce dal tag li.nav_menu__item, allora nascondi il menu a tendina
+    $(".nav_menu__item").on({
+
+        mouseleave: function () {
+            $(this).children(".dropdown").css("display", "none");
+        }
+    });
 
 
 
