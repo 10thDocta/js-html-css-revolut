8b0bc25a334ad87870a9a5bfa0667204e123a159
Davide Preti <davide.preti86@gmail.com>
2020-09-03 16:37:23 +0200

    Iniziata header, stile e logica in jquery

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("nav li a").on({
+
+        mouseenter: function () {
+            $(this).addClass("active");
+        },
+
+        mouseleave: function () {
+            $(this).removeClass("active");
+        }
+
+    });
+    // fine selettore
+
+
+
+
+
+
+})
